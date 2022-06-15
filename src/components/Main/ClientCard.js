3433a85ab9861a0f7b71c0b1ab117ab406da46a1
yaofeng <navy_blue@163.com>
2022-06-15 15:25:27 +0800

    adjust avatar url

--- src/components/Main/ClientCard.js
+++ src/components/Main/ClientCard.js
@@ -1,9 +1,14 @@
 import "./clientcard.css";
+import emily from '../../images/image-emily.jpg';
+import thomas from '../../images/image-thomas.jpg';
+import jennie from '../../images/image-jennie.jpg';
 
 const ClientCard = ({client, index}) => {
+    const imageUrl = index === 0 ? emily : (index === 1 ? thomas : jennie);
+
     return (
         <article className="client-article">
-            <img className="client-article__avatar" src={client.image} alt={client.title} />
+            <img className="client-article__avatar" src={imageUrl} alt={client.title} />
             <p className="client-article__content">{client.content}</p> 
             <h2 className="client-article__name big-text">{client.name}</h2>
             <h3 className="client-article__title">{client.title}</h3>
